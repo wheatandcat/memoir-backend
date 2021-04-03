90f243625986cd5c0a2f7fd7532aa267d494db8c
wheatandcat <wheatandcat@gmail.com>
2021-04-03 13:50:24 +0900

    update ci

--- repository/firebase.go
+++ repository/firebase.go
@@ -5,17 +5,13 @@ import (
 	"os"
 
 	firebase "firebase.google.com/go"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 )
 
 // FirebaseApp Firebase App
 func FirebaseApp(ctx context.Context) (*firebase.App, error) {
-	credentials, err := google.CredentialsFromJSON(ctx, []byte(os.Getenv("FIREBASE_KEYFILE_JSON")))
-	if err != nil {
-		return nil, err
-	}
-	opt := option.WithCredentials(credentials)
+	opt := option.WithCredentialsFile("serviceAccount.json")
+	config := &firebase.Config{ProjectID: os.Getenv("FIREBASE_PROJECT_ID")}
 
-	return firebase.NewApp(ctx, nil, opt)
+	return firebase.NewApp(ctx, config, opt)
 }
