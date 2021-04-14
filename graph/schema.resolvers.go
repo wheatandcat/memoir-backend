e12aecba96a9313f06a9b9de6dd7bb907071280d
wheatandcat <wheatandcat@gmail.com>
2021-04-14 20:38:44 +0900

    add: test by TestGetItemsInPeriod

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,7 +5,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/wheatandcat/memoir-backend/graph/generated"
@@ -143,13 +142,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *queryResolver) ItemsByPeriod(ctx context.Context, input model.InputItemsInPeriod) (*model.ItemsInPeriod, error) {
-	panic(fmt.Errorf("not implemented"))
-}
