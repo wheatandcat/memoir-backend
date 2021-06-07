303c631676dcfda18a56ec6ebda89cf37e895220
Yohei Iino <wheatandcat@gmail.com>
2021-06-07 23:24:12 +0900

    newRelationshipRequestでUser情報を返す (#37)
    * update: CreateRelationshipRequest
    
    * update: CreateRelationshipRequest

--- graph/relationship_request.go
+++ graph/relationship_request.go
@@ -22,7 +22,11 @@ func (g *Graph) CreateRelationshipRequest(ctx context.Context, input model.NewRe
 		return nil, err
 	}
 	if i.UserID == "" {
-		return nil, fmt.Errorf("存在しない招待コードです")
+		return nil, fmt.Errorf("招待コードが見つかりません")
+	}
+
+	if i.UserID == g.UserID {
+		return nil, fmt.Errorf("自身の招待コードです")
 	}
 
 	uuid := g.Client.UUID.Get()
@@ -48,6 +52,13 @@ func (g *Graph) CreateRelationshipRequest(ctx context.Context, input model.NewRe
 	if err = g.App.RelationshipRequestRepository.Create(ctx, g.FirestoreClient, rr); err != nil {
 		return nil, err
 	}
+
+	u, err := g.App.UserRepository.FindByUID(ctx, g.FirestoreClient, i.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	rr.User = u
 
 	return rr, nil
 }
