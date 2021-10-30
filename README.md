2c8f6c8a3aa29fac107d64b3efcb7ca4e768627e
Yohei Iino <wheatandcat@gmail.com>
2021-10-30 23:32:51 +0900

    scenarigo導入 (#87)
    * add: e2e
    
    * add: item by e2e
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions
    
    * update github actions

--- README.md
+++ README.md
@@ -23,55 +23,66 @@ $ yarn graphql-markdown http://localhost:8080/query > schema.md
 
 ## GraphQLスキーマ更新
 
-```
+```:zsh
 $ go run github.com/99designs/gqlgen generate
 ```
 
-## テスト
-
+## ユニットテスト
 
 ### 実行
 moqを生成
-```
+```:zsh
 $ make moqgen
 ```
 
 テストを実行
-```
+```:zsh
 $ go test -race ./...
 ```
 
 ### カバレッジ表示
 
 ### インストール
-```
+```:zsh
 $ brew install k1LoW/tap/octocov
 ```
 
 ### 実行
-```
+```:zsh
 $ go test ./... -coverprofile=coverage.out
 $ octocov
 ```
 
 ### 各ファイルカバレッジ確認
-```
+```:zsh
 $ octocov ls-files
 ```
 
-```
+```:zsh
 $ octocov view graph/invite.go
+```
+
+## E2Eテスト
+
+```:zsh
+$ FIRESTORE_EMULATOR_HOST=localhost:3600 air
+```
+
+```:zsh
+$ cd e2e
+$ make create_loggin_yaml
+$ make local_scenarigo
 ```
 
 ## 手動デプロイ
 
-```
+```:zsh
 $ gcloud app deploy
 ```
 
 ## 本番デプロイ
 
-```
+```:zsh
 $ git checkout main
 $ git pull --ff-only origin main
 $ git tag -a v1.0.0 -m 'リリース内容'
@@ -80,31 +91,50 @@ $ git push origin v1.0.0
 
 ## CI環境
 
+### Firebase トークン
+
+```:zsh
+$ firebase login:ci
+```
 ### レビュー環境
 
-```
+```:zsh
 $ base64 -i serviceAccount.review.json | pbcopy
 ```
 
-```
+```:zsh
 $ base64 -i gcpServiceAccount.review.json | pbcopy
 ```
 
-```
+```:zsh
 $ base64 -i app.yaml | pbcopy
+```
+
+### E2E
+
+```:zsh
+$ base64 -i .env | pbcopy
+```
+
+```:zsh
+$ base64 -i envenb.go | pbcopy
+```
+
+```:zsh
+$ base64 -i e2e/.env | pbcopy
 ```
 
 ### 本番環境
 
-```
+```:zsh
 $ base64 -i serviceAccount.production.json | pbcopy
 ```
 
-```
+```:zsh
 $ base64 -i gcpServiceAccount.production.json | pbcopy
 ```
 
-```
+```:zsh
 $ base64 -i app.production.yaml | pbcopy
 ```
 
---
+++ e2e/script/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	firebase "firebase.google.com/go"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"gopkg.in/yaml.v2"
+)
+
+const verifyCustomTokenURL = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=%s"
+
+type User struct {
+	UserToken string `yaml:"userToken"`
+}
+
+type LoginYaml struct {
+	Vars User
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("読み込み出来ませんでした: %v", err)
+	}
+
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("../serviceAccount.json")
+	config := &firebase.Config{ProjectID: os.Getenv("FIREBASE_PROJECT_ID")}
+	app, err := firebase.NewApp(ctx, config, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	client, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	token, err := client.CustomToken(ctx, "some-uid")
+	if err != nil {
+		log.Fatalf("error minting custom token: %v\n", err)
+	}
+
+	req, err := json.Marshal(map[string]interface{}{
+		"token":             token,
+		"returnSecureToken": true,
+	})
+	if err != nil {
+		log.Fatalf("error minting custom token: %v\n", err)
+	}
+
+	apiKey := os.Getenv("FIREBASE_API_KEY")
+
+	resp, err := postRequest(fmt.Sprintf(verifyCustomTokenURL, apiKey), req)
+	if err != nil {
+		log.Fatalf("error minting custom token: %v\n", err)
+	}
+
+	var respBody struct {
+		IDToken string `json:"idToken"`
+	}
+	if err := json.Unmarshal(resp, &respBody); err != nil {
+		log.Fatalf("error minting custom token: %v\n", err)
+	}
+
+	ly := LoginYaml{}
+	ly.Vars.UserToken = respBody.IDToken
+
+	if err := WriteOnFile("./login.yaml", ly); err != nil {
+		log.Fatalf("WriteOnFile: %v\n", err)
+	}
+
+	os.Chmod("./login.yaml", 0777)
+}
+
+func postRequest(url string, req []byte) ([]byte, error) {
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(req))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected http status code: %d", resp.StatusCode)
+	}
+	return ioutil.ReadAll(resp.Body)
+}
+
+func WriteOnFile(fileName string, data interface{}) error {
+	// ここでデータを []byte に変換しています。
+	buf, err := yaml.Marshal(data)
+	if err != nil {
+		return err
+	}
+	// []byte をファイルに上書きしています。
+	err = ioutil.WriteFile(fileName, buf, os.ModeExclusive)
+	if err != nil {
+		return err
+	}
+	return nil
+}
