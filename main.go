c790adae9d4a4f32250f31c5158ae60f7c166d60
wheatandcat <wheatandcat@gmail.com>
2025-07-19 10:01:19 +0900

    fix: e2e

--- main.go
+++ main.go
@@ -147,7 +147,7 @@ func main() {
 		App:             app,
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolver}))
+	srv := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: resolver}))
 
 	srv.Use(app_trace.NewGraphQLTracer(tr))
 
